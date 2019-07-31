d146fff57302b5b2a671c5ccd4cebf8fe3da57ad
Min Zhang <zm@tuia.cn>
2019-07-31 17:02:58 +0800

    1

--- hooks/index.js
+++ hooks/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-let preCommitFile = fs.readFileSync(path.join(__dirname, './pre-commit'))
+let preCommitFile = fs.readFileSync(path.join(__dirname, './pre-push'))
 
-fs.writeFileSync('.git/hooks/pre-commit', preCommitFile, {
+fs.writeFileSync('.git/hooks/pre-push', preCommitFile, {
     encoding: 'utf8',
     mode: 0o777
 })
